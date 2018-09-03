72b98fba033ae73e5b9aa631c359402588c8a9eb
Chang Feng <chang_196700@hotmail.com>
2018-09-03 20:53:08 +0800

    Change abbrlink use date
    Signed-off-by: Chang Feng <chang_196700@hotmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 'use strict';
 
+var assign = require('deep-assign');
+
 var hexo = hexo || {};
+
+hexo.config.abbrlink = assign({
+    alg: 'crc16',
+    rep: 'dec',
+    skip_draft: true,
+    check: false
+}, hexo.config.abbrlink);
 
 hexo.extend.filter.register('before_post_render', require('./lib/logic'), 15);
